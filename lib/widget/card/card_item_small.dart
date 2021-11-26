9fd9a3989eb1dfb7e6faf67a02d670b517345c11
Your Name <lethelu97@gmail.com>
2021-11-26 17:54:42 +0700

    demo

---
+++ lib/widget/card/card_item_small.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget cardItemSmall(){
+  return Container(
+    height: 105,
+    width: 249,
+    decoration: BoxDecoration(
+      color: Colors.teal[300],
+      borderRadius: BorderRadius.all(Radius.circular(18))
+    ),
+    child: Row(
+      children: [
+
+      ],
+    ),
+  );
+}

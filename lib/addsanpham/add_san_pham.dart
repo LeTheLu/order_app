f161f07fb4148c832932eb9acb5d5c022b1d04b8
LeTheLu <48428230+LeTheLu@users.noreply.github.com>
2021-12-06 20:38:12 +0700

    đ

---
+++ lib/addsanpham/add_san_pham.dart
@@ -0,0 +1,130 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:order_app/addsanpham/data.dart';
+import 'package:order_app/utils/colors.dart';
+import 'package:order_app/widgets/button/button.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const AddSanPham());
+}
+
+class AddSanPham extends StatefulWidget {
+  const AddSanPham({Key? key}) : super(key: key);
+
+  @override
+  _AddSanPhamState createState() => _AddSanPhamState();
+}
+
+class _AddSanPhamState extends State<AddSanPham> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController typeController = TextEditingController();
+  TextEditingController img01Controller = TextEditingController();
+  TextEditingController img02Controller = TextEditingController();
+  TextEditingController img03Controller = TextEditingController();
+
+  final FirebaseFirestore _storeFirebase = FirebaseFirestore.instance;
+  String dropdownValue = "drinks";
+  late String collection;
+
+
+@override
+  void initState() {
+  collection = "drinks";
+    super.initState();
+  }
+
+
+  addDrinks(){
+    for (var element in Data.dataDrinks) {
+      addSanPham(element);
+    }
+  }
+  addFoods(){
+    for (var element in Data.dataFoods) {
+      addSanPham(element);
+    }
+  }
+  addFruits(){
+    for (var element in Data.dataFruits) {
+      addSanPham(element);
+    }
+  }
+
+
+  addSanPham(product){
+    _storeFirebase.collection("product").doc("sanpham").collection(collection).add(product).then((value) {
+      print("Done");
+    } ).catchError((e)=> print("Err"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: ColorApp.themeColor),
+              underline: Container(
+                height: 2,
+                color: ColorApp.themeColor,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                  collection = newValue;
+                });
+              },
+              items: <String>['drinks', 'foods', 'fruits']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+                Button(
+                  nameButton: "Add SanPham",
+                  onTap: (){
+                    switch (collection){
+                      case "drinks" : {
+                        addDrinks();
+                        break;
+                      }
+                      case "foods" : {
+                        addFoods();
+                        break;
+                      }
+                      case "fruits" : {
+                        addFruits();
+                        break;
+                      }
+                    }
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/addsanpham/data.dart
@@ -0,0 +1,251 @@
+class Data {
+  static List dataFruits = [
+    {
+      "name": "Táo Mỹ",
+      "price": "4.99",
+      "description":
+          "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+      "img": [
+        "https://suckhoedoisong.qltns.mediacdn.vn/Images/haiyen/2018/12/10/tao.jpg",
+        "https://product.hstatic.net/1000282430/product/tao_xanh_my.jpg",
+        "https://newfreshfoods.com.vn/datafiles/3/2018-02-27/16100958642348_tao-do-my-red-delicious-size-36-44.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Chuối",
+      "price": "2.99",
+      "description":
+          "Chuối là một trong những loại cây lương thực quan trọng nhất trên hành tinh. Nó là loại thực vật thuộc họ Musa có nguồn gốc từ Đông Nam Á và được trồng nhiều ở ...",
+      "img": [
+        "https://kenh14cdn.com/thumb_w/660/2020/6/27/photo-1-15932669450161767331435.jpg",
+        "https://cdn-www.vinid.net/2020/04/bb470641-gi%C3%A1-tr%E1%BB%8B-dinh-d%C6%B0%E1%BB%A1ng-c%E1%BB%A7a-qu%E1%BA%A3-chu%E1%BB%91i.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Ớt Chuông",
+      "price": "3.00",
+      "description":
+          "Ớt chuông có thành phần chất dinh dưỡng khá giàu gồm: vitamin A, vitamin C và các chất dinh dưỡng khác. Hàm lượng vitamin A trong 149 gam chuông xanh cung cấp ...",
+      "img": [
+        "https://cf.shopee.vn/file/769fd1c94cb710e05dc149ab4aaf3bf3",
+        "https://product.hstatic.net/200000356473/product/ot_chuong_do_1_ada676b2a02e406b9812d8ba698ea019_1024x1024.jpg",
+        "https://product.hstatic.net/1000282430/product/ot-chuong-vang_large.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Gừng",
+      "price": "1.20",
+      "description":
+          "Gừng còn có tên khác là sinh khương, can khương, bào khương... Tên khoa học Zingiber officinale Rose, họ Gừng (Zingiberaceae). Được trồng phổ ...",
+      "img": [
+        "https://photo-cms-plo.zadn.vn/w800/Uploaded/2021/xqeioxmrke/2020_03_21/gunglamgiamnguycobenhtim_pycf.jpg",
+        "https://suckhoedoisong.qltns.mediacdn.vn/324455921873985536/2021/10/28/gung-co-tac-dung-gi-2-1635438675606590955706.jpg",
+        "http://vfa.gov.vn/data/images/Pic_2015/gung%201.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Cà Chua",
+      "price": "2.50",
+      "description":
+          "Cà chua rất giàu vitamin A, C, K, vitamin B6, kali, folate, thiamin, magiê, niacin, đồng và phốt pho, là những vi chấtcần thiết để duy trì một ...",
+      "img": [
+        "https://suckhoedoisong.qltns.mediacdn.vn/Images/thanhloan/2016/06/30/ca-chua.jpg",
+        "https://st.quantrimang.com/photos/image/2017/08/26/ca-chua-1.jpg",
+        "https://icdn.dantri.com.vn/thumb_w/640/2019/05/18/ca-chua-1558139604071.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Hạnh Nhân",
+      "price": "4.88",
+      "description":
+          "Hạnh nhân thuộc cây thân gỗ với chiều cao phát triển đến 10m. Thân cây có đường kính lớn đến 30cm. Cành cây non có màu xanh, rồi chuyển sang màu tím dưới sự tác ...",
+      "img": [
+        "https://babymua.com/images/companies/1/_hat-hanh-nhan_1.jpg?1493528195823",
+        "https://namlimxanh.vn/wp-content/uploads/2018/10/cach-dung-hat-hanh-nhan-hieu-qua-va-gia-cua-hat-hanh-nhan-hien-nay-700x438.jpg",
+        "https://madefresh.com.vn/wp-content/uploads/2020/06/nguon-goc-hanh-nhan.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Cam",
+      "price": "1.90",
+      "description":
+          "Cam là quả của nhiều loài cây có múi khác nhau trong họ Rutaceae (xem danh sách các loài thực vật được gọi là cam); nó chủ yếu đề cập đến Citrus × sinensis, ...",
+      "img": [
+        "https://dacsancamvinh.net/wp-content/uploads/2015/06/cong-dung-qua-cam.jpg",
+        "https://hoaquafuji.com/storage/app/media/cam-ruot-do-my-fuji-0-3.jpg",
+        "https://cdn-www.vinid.net/2019/10/an-cam-co-tac-dung-gi-nen-an-cam-luc-nao-tot-nhat.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Nho",
+      "price": "9.00",
+      "description":
+          "Nho là một loại quả mọng lấy từ các loài cây thân leo thuộc chi Nho (Vitis). Quả nho mọc thành chùm từ 6 đến 300 quả, chúng có màu đen, lam, vàng, lục, ...",
+      "img": [
+        "https://photo-cms-viettimes.zadn.vn/666x374/Uploaded/2021/ovhipuo/2019_11_26/1nho_den_6677263_26112019.jpg",
+        "https://hoaquafuji.com/storage/app/media/gia-nho-sua-han-quoc-fuji-7.jpg",
+        "https://minhphuongfruit.com/upload/images/z2009121723954_681c822675eb1a356051dedca56889e9.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Đào",
+      "price": "1.99",
+      "description":
+          "Đào (danh pháp khoa học: Prunus persica) là một loài cây được trồng để lấy quả hay hoa. Nó là một loài cây sớm rụng lá, thân gỗ nhỏ, có thể cao tới 5–10 m.",
+      "img": [
+        "https://suckhoedoisong.qltns.mediacdn.vn/Images/duylinh/2020/05/14/dao_2_resize.jpg",
+        "https://images.baodantoc.vn/uploads/2021/Th%C3%A1ng%204/Ng%C3%A0y_22/Du%20lich%20bac%20Ha/4.JPG",
+        "https://metunhien.vn/wp-content/uploads/sites/1/2019/08/Dao-Nhat-Ban-vinfr.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Mít",
+      "price": "0.99",
+      "description":
+          "Mít (danh pháp hai phần: Artocarpus heterophyllus) là loài thực vật ăn quả, mọc phổ biến ở vùng Đông Nam Á và Brasil. Mít thuộc họ Dâu tằm (Moraceae) và ...",
+      "img": [
+        "https://cdn.tgdd.vn/Files/2019/10/10/1207046/dinh-duong-cua-qua-mit-an-mit-the-nao-cho-dung-cach--1.jpg",
+        "https://vinmec-prod.s3.amazonaws.com/images/20210714_030410_210578_di-ung-mit.max-1800x1800.jpg",
+        "https://vinmec-prod.s3.amazonaws.com/images/20190610_110931_113070_co-nen-an-mit-khi-t.max-1800x1800.jpg"
+      ],
+      "type": "Fruits"
+    },
+    {
+      "name": "Dừa",
+      "price": "3.88",
+      "description":
+          "Dừa, hay cọ dừa, (danh pháp hai phần: Cocos nucifera), là một loài cây trong họ Cau (Arecaceae). Nó cũng là thành viên duy nhất trong chi Cocos và là một ...",
+      "img": [
+        "https://aloduasap.com/wp-content/uploads/2019/11/z1417874272555_5dc2ce0a9e708c99c735315ce47c01b3_f3e3a3f1a7c847249cbce4feca5f26d7_master.jpg",
+        "https://cdn.tgdd.vn/2020/07/content/8-800x800.jpg",
+        "https://centerforhealthreporting.org/wp-content/uploads/2020/05/chua-dau-da-day-bang-qua-dua-co-hieu-qua-khong..jpg"
+      ],
+      "type": "Fruits"
+    }
+  ];
+
+  static List dataFoods = [
+    {
+      "name": "Phở",
+      "price": "4.99",
+      "description": "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+      "img": [
+        "http://c.files.bbci.co.uk/12547/production/_118297057_08f605d2-cd56-4e26-87e2-2313dc09d089.jpg",
+        "https://bepcuatoi.com/wp-content/uploads/2020/08/mon-pho-bo-thom-ngon.png",
+        "https://vcdn-dulich.vnecdn.net/2020/10/01/500px-phohaitrieu-6059-1540791-6860-5415-1601542922.jpg"
+      ],
+      "type": "Foods"
+    },
+    {
+      "name": "Bánh Mì",
+      "price": "2.99",
+      "description": "Chuối là một trong những loại cây lương thực quan trọng nhất trên hành tinh. Nó là loại thực vật thuộc họ Musa có nguồn gốc từ Đông Nam Á và được trồng nhiều ở ...",
+      "img": [
+        "https://vcdn-dulich.vnecdn.net/2020/10/06/banhmi-2678-1585830175-3225-1601963256.png",
+        "https://cdn.huongnghiepaau.com/wp-content/uploads/2019/08/banh-mi-kep-thit-nuong-thom-phuc.jpg",
+        "https://imagevietnam.vnanet.vn/upload/Thumnail/2020/1/14/14012020105222243CV6.jpg"
+      ],
+      "type": "Foods"
+    },
+    {
+      "name": "Cơm Rang",
+      "price": "1.20",
+      "description": "Gừng còn có tên khác là sinh khương, can khương, bào khương... Tên khoa học Zingiber officinale Rose, họ Gừng (Zingiberaceae). Được trồng phổ ...",
+      "img": [
+        "https://www.knorr.com/content/dam/unilever/knorr_world/vietnam/general_image/savoury/other_savoury/teaser_meo_rang_com-1187779.jpg",
+        "https://reviewaz.vn/storage/com-rang-mon-com-an-ngon-tuyet-dinh.JPG",
+        "https://image-us.eva.vn/upload/4-2020/images/2020-12-11/image10-1607667365-797-width640height450.jpg"
+      ],
+      "type": "Foods"
+    },
+    {
+      "name": "Mỳ Ý",
+      "price": "2.50",
+      "description": "Cà chua rất giàu vitamin A, C, K, vitamin B6, kali, folate, thiamin, magiê, niacin, đồng và phốt pho, là những vi chấtcần thiết để duy trì một ...",
+      "img": [
+        "https://cdn.huongnghiepaau.com/wp-content/uploads/2019/01/my-y-chay.jpg",
+        "https://cdn.daynauan.info.vn/wp-content/uploads/2018/03/hinh-mi-y-xot-bo-bam.jpg",
+        "https://giadinh.tv/wp-content/uploads/2016/12/mon-ngon-cuoi-tuan-spagetti-1-e1482265877334.jpg"
+      ],
+      "type": "Foods"
+    },
+    {
+      "name": "Gà Quay",
+      "price": "1.90",
+      "description": "Cam là quả của nhiều loài cây có múi khác nhau trong họ Rutaceae (xem danh sách các loài thực vật được gọi là cam); nó chủ yếu đề cập đến Citrus × sinensis, ...",
+      "img": [
+        "https://cdn.tgdd.vn/2021/03/CookRecipe/GalleryStep/thanh-pham-287.jpg",
+        "https://cdn.tgdd.vn/Files/2020/10/10/1297715/chieu-dai-ca-nha-voi-mon-ga-quay-me-da-gion-thom-ngon-nuc-mui-202010101538080615.jpg",
+        "https://cdn.beptruong.edu.vn/wp-content/uploads/2016/07/ga-quay-ngon.jpg"
+      ],
+      "type": "Foods"
+    }
+  ];
+  static List dataDrinks = [
+    {
+      "name": "CoCaCola",
+      "price": "4.99",
+      "description": "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+      "img": [
+        "https://media-api.advertisingvietnam.com/oapi/v1/media?uuid=public%2Fwp-content%2Fuploads%2F2019%2F06%2F6-quang-cao-dang-nho-cua-coca-cola.jpg&resolution=1440x756&type=image",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/15-09-26-RalfR-WLC-0098.jpg/220px-15-09-26-RalfR-WLC-0098.jpg",
+        "https://blog.bizbooks.vn/wp-content/uploads/2019/04/thuong-hieu-coca-cola.2-jpg.jpg"
+      ],
+      "type": "Drinks"
+    },
+    {
+      "name": "PesiCola",
+      "price": "2.99",
+      "description": "Chuối là một trong những loại cây lương thực quan trọng nhất trên hành tinh. Nó là loại thực vật thuộc họ Musa có nguồn gốc từ Đông Nam Á và được trồng nhiều ở ...",
+      "img": [
+        "https://my-live-05.slatic.net/p/b0213a75c83ee9be7624e65a6cadf376.jpg_1200x1200q80.jpg_.webp",
+        "https://media3.scdn.vn/img4/2020/07_29/9CbFFZCWFpZUHZI0L9kk_simg_de2fe0_500x500_maxb.jpg",
+        "https://vn-test-11.slatic.net/p/mdc/90fe2e038a595f87f31a008bac36ff85.jpg_320x320.jpg"
+      ],
+      "type": "Drinks"
+    },
+    {
+      "name": "Nước Cam",
+      "price": "1.20",
+      "description": "Gừng còn có tên khác là sinh khương, can khương, bào khương... Tên khoa học Zingiber officinale Rose, họ Gừng (Zingiberaceae). Được trồng phổ ...",
+      "img": [
+        "https://photo-cms-plo.zadn.vn/Uploaded/2021/ycivopcg/2020_04_04/hinh-cam-vat_ltvp.jpg",
+        "https://cdn.tgdd.vn/2020/07/CookProductThumb/nuocscam-620x620.jpg",
+        "https://cdn.tgdd.vn/Files/2018/11/27/1134029/cong-dung-cua-nuoc-cam-tuoi-va-cach-bao-quan-nuoc-cam-tot-nhat-7-760x367.jpg"
+      ],
+      "type": "Drinks"
+    },
+    {
+      "name": "Nước Dưa Hấu",
+      "price": "2.50",
+      "description": "Cà chua rất giàu vitamin A, C, K, vitamin B6, kali, folate, thiamin, magiê, niacin, đồng và phốt pho, là những vi chấtcần thiết để duy trì một ...",
+      "img": [
+        "https://image-us.eva.vn/upload/2-2019/images/2019-06-13/nuoc-ep-dua-hau-ngon-giai-nhiet-ngay-he-voi-cach-lam-don-gian-cach-lam-nuoc-ep-dua-hau-5-1560412696-423-width600height450.jpg",
+        "https://viquang.vn/wp-content/uploads/2017/06/n%C6%B0%E1%BB%9Bc-%C3%A9p-d%C6%B0a-hauas.jpg",
+        "https://smoothiedays.com/wp-content/uploads/2020/07/cach-lam-nuoc-ep-dua-hau.png"
+      ],
+      "type": "Drinks"
+    },
+    {
+      "name": "Nước Dừa",
+      "price": "1.90",
+      "description": "Cam là quả của nhiều loài cây có múi khác nhau trong họ Rutaceae (xem danh sách các loài thực vật được gọi là cam); nó chủ yếu đề cập đến Citrus × sinensis, ...",
+      "img": [
+        "https://vinmec-prod.s3.amazonaws.com/images/20200513_094457_911488_loi-ich-cua-nuoc-du.max-1800x1800.jpg",
+        "https://ngonaz.com/wp-content/uploads/2020/10/uong-nuoc-dua-buoi-sang-co-tot-khong-2.jpg",
+        "https://cdn.tgdd.vn/2021/09/CookDish/mach-ban-cach-bao-quan-nuoc-dua-dua-tuoi-got-vo-va-dua-chua-avt-1200x676.jpg"
+      ],
+      "type": "Drinks"
+    }
+  ];
+}
+
